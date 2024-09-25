73e384164822dfd97065756afef34c022712d5d0
vivek <vivekvasant310@gmail.com>
2024-09-25 18:18:03 +0530

    final change

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let hamburger = document.getElementById("hamburger")
 let nav = document.getElementById("navigation")
+let chat = document.getElementById("chat")
+let content = document.getElementById("content")
 
 console.log(hamburger)
 hamburger.addEventListener("click",()=>{
     nav.classList.toggle("closed")
+})
+
+chat.addEventListener("click",()=>{
+    content.classList.toggle("chat-open")
 })
